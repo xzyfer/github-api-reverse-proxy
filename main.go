805e16f54bf930930b26068118a86bba500ebdde
Michael Mifsud <xzyfer@gmail.com>
2017-06-29 23:22:56 +1000

    Magic

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-// 	"fmt"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -21,18 +21,18 @@ func main() {
 }
 
 func ProxyFunc(w http.ResponseWriter, r *http.Request) {
-// 	if token, exists := os.LookupEnv("AUTH_TOKEN"); exists {
-// 		r.Header.Set("Authorization", fmt.Sprintf("token %s", token))
-// 	}
+	if token, exists := os.LookupEnv("AUTH_TOKEN"); exists {
+		r.Header.Set("Authorization", fmt.Sprintf("token %s", token))
+	}
 	
-// 	if ua, exists := os.LookupEnv("USER_AGENT"); exists {
-// 		r.Header.Set("User-Agent", ua)
-// 	} else {
+	if ua, exists := os.LookupEnv("USER_AGENT"); exists {
+		r.Header.Set("User-Agent", ua)
+	} else {
 		r.Header.Set("User-Agent", "github-api-reverse-proxy")
-// 	}
+	}
 	
-// 	r.Header.Set("Pragma", "no-cache")
-// 	r.Header.Set("Host", "api.github.com")
+	r.Header.Set("Pragma", "no-cache")
+	r.Host = "api.github.com"
 
 	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
 		Scheme: "https",
